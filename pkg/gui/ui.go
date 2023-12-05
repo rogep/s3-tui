718de4ebdf5b94829009387c368eb6b53f9e96bb
ephraim.rogers <ephraim.rogers@consilium.technology>
2023-12-05 15:09:40 +1030

    fix: made all columns equal width

--- pkg/gui/ui.go
+++ pkg/gui/ui.go
@@ -46,7 +46,7 @@ func CreateGridWithSearch(buckets *tview.List, files *tview.List, preview *tview
 func CreateDefaultGrid(buckets *tview.List, files *tview.List, preview *tview.TextView, footer *tview.TextView) *tview.Grid {
 	grid := tview.NewGrid().
 		SetRows(1, 0, 1).
-		SetColumns(0, -2, 0).
+		SetColumns(0, 0, 0).
 		SetBorders(false).
 		AddItem(tview.NewTextView().
 			SetTextAlign(tview.AlignLeft).
